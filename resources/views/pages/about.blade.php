338ee55a2b5ea6d56fa49f23a03067796150eb5a
chalmie <blairchalmers@gmail.com>
2016-12-15 20:52:18 -0800

    add navbar and head partials

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,9 @@
-@extends('app');
+@extends('app')
 
 @section('content')
-
   <div class="content">
-      <div class="title m-b-md">About {{ $data["firstname"] }}</div>
-        <p>Email: {{ $data["email"] }}</p>
-      </div>
+    <div class="title m-b-md">About {{ $data["firstname"] }}</div>
+      <p>Email: {{ $data["email"] }}</p>
+    </div>
   </div>
-
 @endsection
