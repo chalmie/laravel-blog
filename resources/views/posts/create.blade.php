13b82c7982faa047667e9f2b0a5fab8fe2a0a623
chalmie <blairchalmers@gmail.com>
2016-12-19 17:19:04 -0800

    add form validation to create post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,23 +2,33 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheets')
+  {{!! Html::style('css/parsley.css') !!}}
+@endsection
+
 @section('content')
 
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h1>Create New Post</h1>
       <hr>
-      {!! Form::open(['route' => 'posts.store']) !!}
+      {!! Form::open(['route' => 'posts.store', 'data-parsley-validate' => '']) !!}
         {{ Form::label('title', 'Title:') }}
-        {{ Form::text('title', null, array('class' => 'form-control')) }}
+        {{ Form::text('title', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{ Form::label('body', 'Body:') }}
-        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+        {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{-- inline styling needs to moved to css --}}
         {{ Form::submit('Create Post', array('class' => 'btn btn-info btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
       {!! Form::close() !!}
     </div>
   </div>
+
+@endsection
+
+@section('scripts')
+
+  {{!! Html::script('js/parsley.min.js') !!}}
 
 @endsection
