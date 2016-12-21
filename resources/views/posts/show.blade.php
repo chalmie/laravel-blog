9c146976a42bb5bffcf34e26cbfda6c24af97a5a
chalmie <blairchalmers@gmail.com>
2016-12-20 18:16:55 -0800

    display saved posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,34 @@
 
 @section('content')
 
-  <p class="lead">This is the post</p>
+  <div class="row">
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
+
+      <p class="lead">{{ $post->body }}</p>
+    </div>
+    <div class="col-md-4">
+      <div class="well">
+        <dl class="dl-horizontal">
+          <dt>Created at:</dt>
+          <dt>Time:</dt>
+        </dl>
+        <dl class="dl-horizontal">
+          <dt>Updated at:</dt>
+          <dt>Time:</dt>
+        </dl>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+
+          </div>
+          <div class="col-sm-6">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 @endsection
