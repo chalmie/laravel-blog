66d550e2b6e8c153d1ef44ee8b5f018bc12ff931
chalmie <blairchalmers@gmail.com>
2016-12-27 21:51:16 -0800

    display all blog posts and fix active navbar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,7 +11,36 @@
     <div class="col-md-2">
       <a href="{{ route('posts.create') }}" class="btn btn-lg btn-block btn-info btn-h1-spacing">Create New Post</a>
     </div>
-    <hr>
+    <div class="col-md-12">
+      <hr>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <table class="table">
+        <thead>
+          <th>#</th>
+          <th>Title</th>
+          <th>Body</th>
+          <th>Created At</th>
+          <th></th>
+        </thead>
+        <tbody>
+
+          @foreach($posts as $post)
+            <tr>
+              <td>{{ $post->id }} </td>
+              <td> {{ $post->title }} </td>
+              <td> {{substr($post->body,0,50) }}{{ strlen($post->body) > 50 ? "..." : "" }} </td>
+              <td> {{ date('M j, Y', strtotime($post->created_at)) }} </td>
+              <td><a href="{{ route('posts.show', $post->id )}}" class="btn btn-default">View</a><a href="{{ route('posts.edit', $post->id )}}" class="btn btn-default">Edit</a></td>
+             </tr>
+          @endforeach
+
+        </tbody>
+      </table>
+    </div>
   </div>
 
 @endsection
