0e88f4c0457881c19a952e247581390d39de21df
chalmie <blairchalmers@gmail.com>
2017-01-28 23:12:58 -0800

    display all categories

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+
+@section('title', '| All Categories')
+
+@section('content')
+
+  <div class="row">
+    <div class="col-md-8">
+      <h1>Categories</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($categories as $category)
+            <tr>
+              <th>{{ $category->id }}</th>
+              <td>{{ $category->name }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+@endsection
