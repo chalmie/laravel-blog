434cecf8b1a9acd1415feb12099d501e46a5efac
chalmie <blairchalmers@gmail.com>
2017-01-28 21:36:48 -0800

    hiding and showing authenticated links

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -16,31 +16,32 @@
       <ul class="nav navbar-nav navbar-right">
         <li class="{{ Request::is('about') ? "active" : "" }}"><a href="/about">About</a></li>
         <li class="{{ Request::is('contact') ? "active" : "" }}"><a href="/contact">Contact</a></li>
-        <li class="dropdown">
-          <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="{{ Request::is('blog') ? "active" : "" }}"><a href="/blog">Archive</a></li>
-            <li role="separator" class="divider"></li>
-                @if (Route::has('login'))
-                  @if (Auth::check())
-                    <li><a href="{{ route('posts.index') }}">Admin</a></li>
-                    <li>
-                        <a href="{{ url('/logout') }}"
-                            onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
+        @if (Route::has('login'))
+          @if (Auth::check())
+          <li class="dropdown">
+            <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/blog">Archive</a></li>
+              <li><a href="{{ route('posts.index') }}">Admin</a></li>
+              <li role="separator" class="divider"></li>
+                <li>
+                    <a href="{{ url('/logout') }}"
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
 
-                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                  @else
-                      <li><a href="{{ url('/login') }}">Sign In</a></li>
-                  @endif
-                @endif
-          </ul>
-        </li>
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+              @else
+                  <li><a href="{{ url('/login') }}">Sign In</a></li>
+
+            </ul>
+          </li>
+        @endif
+      @endif
       </ul>
     </div>
     <!-- /.navbar-collapse -->
