d810ea4c895acf2085e7633e52b392d80cd1b48c
chalmie <blairchalmers@gmail.com>
2017-03-05 21:42:25 -0800

    author detail view

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -48,7 +48,8 @@ class TagController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.show')->withTag($tag);
     }
 
     /**
